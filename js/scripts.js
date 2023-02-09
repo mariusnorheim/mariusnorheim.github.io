7ccdbdf9315bda3a8c4d08a2c6dbef15b9060f86
Marius Norheim <marius@mariusnorheim.dev>
2023-02-09 17:46:01 +0100

    Added changes to experience and skills

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
     * Start Bootstrap - Resume v1.0.0 (undefined)
-    * Copyright 2013-2022 undefined
+    * Copyright 2013-2023 undefined
     * Licensed under undefined (https://github.com/StartBootstrap/resume/blob/master/LICENSE)
     */
     (function ($) {
