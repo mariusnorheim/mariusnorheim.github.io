5cc47ed4d9680bc1c2c3d447c23ae08613d04d1e
Marius Norheim <marius@mariusnorheim.dev>
2024-02-16 02:06:42 +0100

    Added gallery

--- js/scripts.js
+++ js/scripts.js
@@ -40,3 +40,13 @@
         target: "#sideNav",
     });
 })(jQuery); // End of use strict
+
+$(document).ready(function() {
+    $('.thumbnail img').on('click', function() {
+      var src = $(this).attr('src');
+      var alt = $(this).attr('alt');
+      $('#modalImage').attr('src', src);
+      $('#modalImage').attr('alt', alt);
+      $('#imageModal .modal-title').text(alt); // Set alt text as modal title
+    });
+  });
